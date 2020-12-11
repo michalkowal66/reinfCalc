e8f986a1a5229c633a1f7dc75780d54edf0338fb
Michal Kowal <michal.kowal.66@gmail.com>
2020-12-11 16:03:15 +0100

    Add welcome screen buttons actions

--- app/main.py
+++ app/main.py
@@ -11,6 +11,31 @@ class Main(QtWidgets.QMainWindow):
 
     def setupUi(self):
         self.ui.setupUi(self)
+        self.ui.plate_btn.clicked.connect(self.newPlate)
+        self.ui.beam_btn.clicked.connect(self.newBeam)
+        self.ui.column_btn.clicked.connect(self.newColumn)
+        self.ui.foot_btn.clicked.connect(self.newFoot)
+        self.ui.actionClose.triggered.connect(self.close)
+        self.ui.actionNew.triggered.connect(self.showWelcomePage)
+
+    def newPlate(self):
+        self.ui.stackedWidget.setCurrentIndex(1)
+        self.ui.elements_tabs.setCurrentIndex(0)
+
+    def newBeam(self):
+        self.ui.stackedWidget.setCurrentIndex(1)
+        self.ui.elements_tabs.setCurrentIndex(1)
+
+    def newColumn(self):
+        self.ui.stackedWidget.setCurrentIndex(1)
+        self.ui.elements_tabs.setCurrentIndex(2)
+
+    def newFoot(self):
+        self.ui.stackedWidget.setCurrentIndex(1)
+        self.ui.elements_tabs.setCurrentIndex(3)
+
+    def showWelcomePage(self):
+        self.ui.stackedWidget.setCurrentIndex(0)
 
 
 if __name__ == "__main__":
