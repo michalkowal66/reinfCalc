f2cb357d8ab46725c20ef03cbcb716bbb83bf449
Michal Kowal <michal.kowal.66@gmail.com>
2020-12-11 16:01:54 +0100

    Change window init structure

--- app/main.py
+++ app/main.py
@@ -1,12 +1,16 @@
 from templates.ui import Ui_MainWindow
 from PyQt5 import QtCore, QtGui, QtWidgets
 
+
 class Main(QtWidgets.QMainWindow):
 
-    def __init__(self, parent=None):
-        super().__init__(parent)
-        ui = Ui_MainWindow()
-        ui.setupUi(self)
+    def __init__(self):
+        super().__init__()
+        self.ui = Ui_MainWindow()
+        self.setupUi()
+
+    def setupUi(self):
+        self.ui.setupUi(self)
 
 
 if __name__ == "__main__":
